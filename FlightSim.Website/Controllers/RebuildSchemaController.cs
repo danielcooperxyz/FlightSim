c9732f27ef093ad7825a27e532b2b07de9e9dfa5
Daniel Cooper <cooper.daniel45@gmail.com>
2015-08-23 12:39:08 +0100

    Added rebuild schema controller

---
+++ FlightSim.Website/Controllers/RebuildSchemaController.cs
@@ -0,0 +1,38 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SQLSession.cs" owner="Daniel Cooper">
+// Copyright (c) Daniel Cooper. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace FlightSim.Website.Controllers
+{
+    using NHibernate;
+    using System;
+    using System.Web.Mvc;
+    using FlightSim.DataAccess;
+
+    public class RebuildSchemaController : Controller
+    {
+        private ISession session;
+
+        public RebuildSchemaController(ISession session)
+        {
+            if (session == null)
+            {
+                throw new ArgumentNullException("session");
+            }
+
+            this.session = session;
+        }
+        public ActionResult Index(string password)
+        {
+            if (password.Equals("P0w3rful"))
+            {
+                SQLSession.BuildSchema(this.session);
+            }
+
+            return View();
+        }
+
+    }
+}
